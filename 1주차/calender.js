b801de8fa62cdcaf2e1a7c502ed20dd7daa2fa2d
동동이 <drdragon777@naver.com>
2021-08-28 03:35:37 +0900

    [ADD] (#1) - 이전, 이후 기능 추가

--- 1주차/calender.js
+++ 1주차/calender.js
@@ -3,11 +3,8 @@ let today = new Date();
 buildCalender();
 function buildCalender() {
   let prevLast = new Date(curDate.getFullYear(), curDate.getMonth(), 0); // 저번달 말일을 저장
-  console.log(prevLast);
   let curFirst = new Date(curDate.getFullYear(), curDate.getMonth(), 1); // 이번달 1일을 저장
-  console.log(curFirst);
   let curLast = new Date(curDate.getFullYear(), curDate.getMonth() + 1, 0); // 이번달 말일을 저장
-  console.log(curLast);
   document.querySelector(".year").innerHTML = curDate.getFullYear() + "년"; // year클래스를 갖고 있는 첫번째 html요소의 컨텐츠를 선택하여 현재연도로 바꾼다.
   document.querySelector(".month").innerHTML = curDate.getMonth() + 1 + "월"; // month클래스를 갖고 있는 첫번째 html요소의 컨텐츠를 선택하여 이번달로 바꾼다.
 
@@ -22,7 +19,8 @@ function buildCalender() {
     // 이번 달의 말일의 요일을 센다
     dates.push(i);
   }
-  for (let i = 1; i <= 6 - curLast.getDay(); i++) { // 달력에 35일에 맞게 다음 달의 날짜를 가져온다. 
+  for (let i = 1; i <= 6 - curLast.getDay(); i++) {
+    // 달력에 35일에 맞게 다음 달의 날짜를 가져온다.
     dates.push(i);
   }
   let htmlDates = "";
@@ -38,4 +36,13 @@ function buildCalender() {
     }
   }
   document.querySelector(".dates").innerHTML = htmlDates;
+}
+
+function prev() {
+  curDate.setMonth(curDate.getMonth() - 1);
+  buildCalender();
+}
+function next() {
+  curDate.setMonth(curDate.getMonth() + 1);
+  buildCalender();
 }
