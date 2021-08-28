00d6d6a08499e80cd2933f64c344b471d4bc1549
동동이 <drdragon777@naver.com>
2021-08-28 15:02:03 +0900

    [ADD] (#1) - month 영문 문자열로 바꾸기

--- 1주차/calender.js
+++ 1주차/calender.js
@@ -2,11 +2,26 @@ let curDate = new Date();
 let today = new Date();
 buildCalender();
 function buildCalender() {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   let prevLast = new Date(curDate.getFullYear(), curDate.getMonth(), 0); // 저번달 말일을 저장
   let curFirst = new Date(curDate.getFullYear(), curDate.getMonth(), 1); // 이번달 1일을 저장
   let curLast = new Date(curDate.getFullYear(), curDate.getMonth() + 1, 0); // 이번달 말일을 저장
   document.querySelector(".year").innerHTML = curDate.getFullYear() + "."; // year클래스를 갖고 있는 첫번째 html요소의 컨텐츠를 선택하여 현재연도로 바꾼다.
-  document.querySelector(".month").innerHTML = curDate.getMonth() + 1 + "."; // month클래스를 갖고 있는 첫번째 html요소의 컨텐츠를 선택하여 이번달로 바꾼다.
+  document.querySelector(".month").innerHTML =
+    monthNames[curDate.getMonth()] + "."; // month클래스를 갖고 있는 첫번째 html요소의 컨텐츠를 선택하여 이번달로 바꾼다.
 
   let dates = [];
   let prevCount = 0;
