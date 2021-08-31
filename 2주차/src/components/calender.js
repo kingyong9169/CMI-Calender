2f8fb60aa3d30bd6deaa0f283bf6f80e123ceb6f
동동이 <drdragon777@naver.com>
2021-08-31 22:11:50 +0900

    [ADD] (#2) - calender 컴포넌트 추가

---
+++ 2주차/src/components/calender.js
@@ -0,0 +1,42 @@
+import { CalenderHeader } from "./CalenderHeader";
+import { htmlDom } from "../utls/htmlDom";
+
+export default class Calender {
+  $target; // target
+  $state = {}; // 변화 감지
+  constructor($target, $props, $state) {
+    // target에 해당하는 것에 대한 생성자 함수
+    this.$target = $target ? $target : htmlDom(`<div class="calender"></div>`);
+    if (!$target) $props.appendChild(this.$target);
+    console.log(this.$target);
+    this.$state = $state;
+    this.setup();
+    this.render();
+  }
+  setup() {
+    // 오버라이딩
+    this.$state = { DISPLAYNUM: 42, curDate: new Date(), tody: new Date() };
+  }
+
+  template() {
+    // 새로 렌더링 될 html 부분
+    return "";
+  }
+
+  setEvent() {
+    // 오버라이딩
+  }
+
+  setState(newState) {
+    this.$state = { ...this.$state, ...newState }; // 기존 상태와 새로 생기는 상태를 하나의 상태로하여 다시 렌더링
+    this.render();
+  }
+
+  render() {
+    // state변경 혹은 이벤트 발생 시 template에 있는 내용으로 다시 렌더링
+    this.$target.innerHTML = this.template();
+
+    new CalenderHeader({ $props: this.$target });
+    this.setEvent();
+  }
+}
