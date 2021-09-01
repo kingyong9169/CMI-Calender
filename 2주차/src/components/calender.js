e2b66a330d7df090da61e7509cbe38511c573877
동동이 <drdragon777@naver.com>
2021-09-01 20:54:09 +0900

    [ADD] (#2) - add Calender state

--- 2주차/src/components/calender.js
+++ 2주차/src/components/calender.js
@@ -1,22 +1,51 @@
 import CalenderHeader from "./CalenderHeader.js";
+import CalenderMain from "./CalenderMain.js";
 import { htmlDom } from "../utils/htmlDom.js";
 export default class Calender {
   $target; // target
   $state = {}; // 변화 감지
   constructor({ $target, $parent, $state }) {
     // target에 해당하는 것에 대한 생성자 함수
-    console.log($target);
-    console.log($parent);
     this.$target = $target ? $target : htmlDom(`<div class="calender"></div>`);
     if (!$target) $parent.appendChild(this.$target);
-    console.log(this.$target);
     this.$state = $state;
     this.setup();
     this.render();
   }
   setup() {
     // 오버라이딩
-    this.$state = { DISPLAYNUM: 42, curDate: new Date(), tody: new Date() };
+    const newState = {
+      monthNames: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+      prevLast: new Date(
+        this.$state.curDate.getFullYear(),
+        this.$state.curDate.getMonth(),
+        0
+      ), // 저번달 말일을 저장
+      curFirst: new Date(
+        this.$state.curDate.getFullYear(),
+        this.$state.curDate.getMonth(),
+        1
+      ), // 이번달 1일을 저장
+      curLast: new Date(
+        this.$state.curDate.getFullYear(),
+        this.$state.curDate.getMonth() + 1,
+        0
+      ), // 이번달 말일을 저장 };
+    };
+    this.$state = { ...this.$state, ...newState };
   }
 
   template() {
@@ -38,6 +67,7 @@ export default class Calender {
     this.$target.innerHTML = this.template();
 
     new CalenderHeader({ $parent: this.$target, $state: this.$state });
+    new CalenderMain({ $parent: this.$target, $state: this.$state });
     this.setEvent();
   }
 }
