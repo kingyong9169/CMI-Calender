3b506c0912d231c214f1e5ae935a7534f4f9aae0
동동이 <drdragon777@naver.com>
2021-09-04 05:21:01 +0900

    [RFR] (#2) - observable 최적화

--- 2주차/src/core/observer.js
+++ 2주차/src/core/observer.js
@@ -1,32 +1,40 @@
 let currentObserver = null;
 
+const debounceFrame = (callback) => {
+  let currentCallback = -1;
+  return () => {
+    cancelAnimationFrame(currentCallback);
+    currentCallback = requestAnimationFrame(callback);
+  };
+};
+
 export const observe = (fn) => {
-  currentObserver = fn;
-  console.log(fn);
+  currentObserver = debounceFrame(fn);
+  console.log(debounceFrame(fn));
   fn();
   currentObserver = null;
 };
 
 export const observable = (obj) => {
-  Object.keys(obj).forEach((key) => {
-    let _value = obj[key];
-    const observers = new Set();
+  const observerMap = {};
 
-    Object.defineProperty(obj, key, {
-      get() {
-        if (currentObserver) observers.add(currentObserver);
-        return _value;
-      },
-
-      set(value) {
-        console.log(key, !value.getMonth, value === _value);
-        if (!value.getMonth && _value === value) return;
-        if (!value.getMonth && JSON.stringify(_value) === JSON.stringify(value))
-          return;
-        _value = value;
-        observers.forEach((fn) => fn());
-      },
-    });
+  return new Proxy(obj, {
+    get(target, name) {
+      observerMap[name] = observerMap[name] || new Set();
+      if (currentObserver) observerMap[name].add(currentObserver);
+      return target[name];
+    },
+    set(target, name, value) {
+      console.log(name, !value.getMonth, target[name] === value);
+      if (!value.getMonth && target[name] === value) return true;
+      if (
+        !value.getMonth &&
+        JSON.stringify(target[name]) === JSON.stringify(value)
+      )
+        return true;
+      target[name] = value;
+      observerMap[name].forEach((fn) => fn());
+      return true;
+    },
   });
-  return obj;
 };
