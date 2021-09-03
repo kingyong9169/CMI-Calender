044601b7acbacbd363563bef0a06b117276c0467
동동이 <drdragon777@naver.com>
2021-09-03 22:58:10 +0900

    [DEL] (#2) - subscribe 제거

--- 2주차/src/core/Store.js
+++ 2주차/src/core/Store.js
@@ -1,4 +1,4 @@
-import { observable, observe } from "./observer.js";
+import { observable } from "./observer.js";
 
 export const createStore = (reducer) => {
   // reducer가 실행될 때 반환하는 객체(state)를 observable로 만들어야 한다.
@@ -8,7 +8,12 @@ export const createStore = (reducer) => {
   const frozenState = {};
   Object.keys(state).forEach((key) => {
     Object.defineProperty(frozenState, key, {
-      get: () => state[key], // get만 정의하여 set을 하지 못하도록 만드는 것이다.
+      get: () => {
+        // if (state[key].getMonth) {
+        //   return new Date(state[key]);
+        // }
+        return state[key];
+      }, // get만 정의하여 set을 하지 못하도록 만드는 것이다.
     });
   });
 
@@ -23,13 +28,8 @@ export const createStore = (reducer) => {
     }
   };
 
-  const subscribe = (callback) => {
-    console.log(callback);
-    observe(callback);
-  };
-
   const getState = () => frozenState;
 
   // subscribe는 observe로 대체한다.
-  return { subscribe, getState, dispatch };
+  return { getState, dispatch };
 };
