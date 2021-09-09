0f33cadf6cdebfa3e84cf75f832353b039fc03f0
동동이 <drdragon777@naver.com>
2021-09-10 01:55:35 +0900

    [ADD] (#3) - add CalenderHeader useContext logic

--- week-3/src/components/CalenderHeader/index.jsx
+++ week-3/src/components/CalenderHeader/index.jsx
@@ -1,20 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { CalenderContext, SetCalenderContext } from "../Calender";
 import StyledCalenderHeader from "./style";
 
 export default function CalenderHeader() {
-  const prev = () => {};
-  const next = () => {};
+  const Calender = useContext(CalenderContext);
+  const setCalender = useContext(SetCalenderContext);
+  const { curDate, monthNames } = Calender;
+
+  const prev = () => {
+    setCalender({
+      ...Calender,
+      curDate: new Date(curDate.setMonth(curDate.getMonth() - 1)),
+    });
+  };
+  const next = () => {
+    setCalender({
+      ...Calender,
+      curDate: new Date(curDate.setMonth(curDate.getMonth() + 1)),
+    });
+  };
+
+  useEffect(() => {
+    setCalender({
+      ...Calender,
+      prevLast: new Date(curDate.getFullYear(), curDate.getMonth(), 0),
+      curFirst: new Date(curDate.getFullYear(), curDate.getMonth(), 1),
+      curLast: new Date(curDate.getFullYear(), curDate.getMonth() + 1, 0),
+    });
+  }, [curDate]);
+
   return (
     <StyledCalenderHeader>
-      <header className="header">
-        <button className="prev" onClick={prev}>
-          <i className="far fa-arrow-alt-circle-left"></i>
-        </button>
-        <div className="currentMonth"></div>
-        <button className="next" onClick={next}>
-          <i className="far fa-arrow-alt-circle-right"></i>
-        </button>
-      </header>
+      <button className="prev" onClick={prev}>
+        <i className="far fa-arrow-alt-circle-left"></i>
+      </button>
+      <div className="currentMonth">
+        {monthNames[curDate.getMonth()] + ". " + curDate.getFullYear() + ". "}
+      </div>
+      <button className="next" onClick={next}>
+        <i className="far fa-arrow-alt-circle-right"></i>
+      </button>
     </StyledCalenderHeader>
   );
 }
--- week-3/src/components/CalenderHeader/style.jsx
+++ week-3/src/components/CalenderHeader/style.jsx
@@ -1,25 +1,23 @@
 import styled from "styled-components";
 
 const CalenderHeader = styled.header`
-  .header {
-    justify-content: center;
-    margin: auto;
-    display: flex;
-    .prev,
-    .next {
-      color: var(--bg);
-      padding: auto;
-      font-size: 2rem;
-      border: none;
-      outline: none;
-      background-color: transparent;
-    }
-    .currentMonth {
-      color: var(--bg);
-      text-align: center;
-      width: 10rem;
-      margin: auto 2rem;
-    }
+  justify-content: center;
+  margin: auto;
+  display: flex;
+  .prev,
+  .next {
+    color: var(--bg);
+    padding: auto;
+    font-size: 2rem;
+    border: none;
+    outline: none;
+    background-color: transparent;
+  }
+  .currentMonth {
+    color: var(--bg);
+    text-align: center;
+    width: 10rem;
+    margin: auto 2rem;
   }
 `;
 
