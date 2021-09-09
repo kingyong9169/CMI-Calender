c5fa31bf2e55311dc04d9df07eb5c0ac9c222c10
동동이 <drdragon777@naver.com>
2021-09-09 23:30:38 +0900

    [ADD] (#3) - add global styles

---
+++ week-3/src/styles/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const App = styled.div`
+  .calenderTitle {
+    text-align: center;
+    font-size: 4rem;
+    color: var(--bg);
+  }
+`;
+
+export default App;
