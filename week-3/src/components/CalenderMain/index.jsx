f3a216ac1aec79aba13fc42e477bf8c3483edb66
동동이 <drdragon777@naver.com>
2021-09-10 01:55:51 +0900

    [ADD] (#3) - add CalenderMain markup

--- week-3/src/components/CalenderMain/index.jsx
+++ week-3/src/components/CalenderMain/index.jsx
@@ -5,20 +5,16 @@ import StyledCalenderMain from "./style";
 export default function CalenderMain() {
   return (
     <StyledCalenderMain>
-      <main className="main">
-        <ul className="days">
-          <li className="day">Sun</li>
-          <li className="day">Mon</li>
-          <li className="day">Tue</li>
-          <li className="day">Wed</li>
-          <li className="day">Thu</li>
-          <li className="day">Fri</li>
-          <li className="day">Sat</li>
-        </ul>
-        <ul className="dates">
-          <CalenderDates />
-        </ul>
-      </main>
+      <ul className="days">
+        <li className="day">Sun</li>
+        <li className="day">Mon</li>
+        <li className="day">Tue</li>
+        <li className="day">Wed</li>
+        <li className="day">Thu</li>
+        <li className="day">Fri</li>
+        <li className="day">Sat</li>
+      </ul>
+      <CalenderDates />
     </StyledCalenderMain>
   );
 }
--- week-3/src/components/CalenderMain/style.jsx
+++ week-3/src/components/CalenderMain/style.jsx
@@ -1,34 +1,34 @@
 import styled from "styled-components";
 
 const CalenderMain = styled.main`
-  .main {
-    background-color: var(--bg);
-    width: 40rem;
-    margin-top: 1rem;
+  background-color: var(--bg);
+  width: 40rem;
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  .days {
+    padding: auto;
+    height: 3.5rem;
+    border-bottom: 2px solid var(--line);
+    text-align: center;
     display: flex;
-    flex-direction: column;
-    .days {
-      padding: auto;
-      height: 3.5rem;
-      border-bottom: 2px solid var(--line);
-      text-align: center;
-      display: flex;
-      flex-wrap: wrap;
-      width: 100%;
-      .day {
-        padding: 1rem;
-        margin: 0;
-        width: calc(100% / 7);
-        display: inline;
-        box-sizing: border-box;
-        border-right: 1px solid var(--line);
-        border-top: 1px solid var(--line);
-      }
+    flex-wrap: wrap;
+    width: 100%;
+    .day {
+      padding: 1rem;
+      margin: 0;
+      width: calc(100% / 7);
+      display: inline;
+      box-sizing: border-box;
+      border-right: 1px solid var(--line);
+      border-top: 1px solid var(--line);
     }
-    .dates {
-      width: 100%;
-      display: flex;
-      flex-wrap: wrap;
+    .day:nth-child(7n + 1) {
+      color: var(--sun);
+      border-left: 1px solid var(--line);
+    }
+    .day:nth-child(7n) {
+      color: var(--sat);
     }
   }
 `;
